cc7704efb0a5c9f95721745d0ccc4dc01e47a4f1
TClion <763242665@qq.com>
2017-11-16 01:28:25 +0800

    Update Two_Sum.py

--- Two_Sum.py
+++ Two_Sum.py
@@ -3,7 +3,7 @@ class Solution(object):
         i = 0
         while i < len(nums):
             nums2 = nums[i+1:]
-            num = target - nums[i]
-            if num in nums2:
-                return [i,nums.index(num,i+1)]
+            sec_num = target - nums[i]
+            if sec_num in nums2:
+                return [i,nums.index(sec_num,i+1)]
             i = i + 1
